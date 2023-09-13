c3eba52568c8076609c46c45bae2013a38e32b50
Meseret Alemnew <84620197+mess-ale@users.noreply.github.com>
2023-09-13 15:18:23 +0300

    Add files via upload

---
+++ src/components/Body1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import hom from '../assets/homepage.jpg'
+import './Body1Styles.css'
+
+const Body1 = () => {
+  return (
+    <div className='heading'>
+        <img src={hom} alt='home-img'></img>
+    </div>
+  )
+}
+
+export default Body1
---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import Body1 from '../components/Body1'
+
+const Home = () => {
+  return (
+    <div>
+        <Nav />
+        <Body1 />
+    </div>
+  )
+}
+
+export default Home
